f003b4c5968d35825b2478038c8a56e8650af047
WalkyLemos <walkylemos47@gmail.com>
2023-08-06 14:38:57 -0300

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// eventos.js
+
+// Función que muestra una alerta al cargar la página
+function saludar() {
+    alert('¡Hola! Bienvenido a mi página.');
+}
+
+// Llamada a la función saludar() cuando se cargue la página
+window.onload = saludar;
